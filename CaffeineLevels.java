e610f534b6f35496a1716221bd68170b8643ed48
lJamesW <81941849+lJamesW@users.noreply.github.com>
2021-04-05 21:29:28 +1000

    Update CaffeineLevels.java

--- CaffeineLevels.java
+++ CaffeineLevels.java
@@ -6,19 +6,19 @@ public class LabProgram {
       double caffeineMg; 
       double SixHours;
       double TewleveHours;
-      double TweentyFourHours;
+      double TwentyFourHours;
       
       caffeineMg = scnr.nextDouble(); 
       
       SixHours = caffeineMg/2;
       TewleveHours = SixHours/2;
-      TweentyFourHours = TewleveHours/4;
+      TwentyFourHours = TewleveHours/4;
       
       System.out.printf("After 6 hours: %.2f mg\n", SixHours);
      
       System.out.printf("After 12 hours: %.2f mg\n", TewleveHours);
       
-      System.out.printf("After 24 hours: %.2f mg\n", TweentyFourHours);
+      System.out.printf("After 24 hours: %.2f mg\n", TwentyFourHours);
      
       
       
