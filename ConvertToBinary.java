30d39d174e98186e2163d697a4222d0d62ee40a9
lJamesW <81941849+lJamesW@users.noreply.github.com>
2021-04-05 21:39:00 +1000

    Create ConvertToBinary.java

---
+++ ConvertToBinary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner; 
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      
+      int num = scnr.nextInt();
+      int i;
+      int output1;
+      String output;
+      output = "";
+      
+       while (num > 0) {
+          output = output + (num % 2);
+          num = num/2;
+          
+       }
+      System.out.println(output);
+   }
+}
